638bde72103de7736bc4fb6540b8b2a5106c925f
David Vivier <david.vivier@cpe.fr>
2018-11-07 17:19:09 +0100

    Add mode services, implement Surveillance mode

--- src/mini_projet/scripts/control.py
+++ src/mini_projet/scripts/control.py
@@ -7,7 +7,7 @@ from std_msgs.msg import UInt8
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
-
+from mini_projet.srv import *
 
 BTN_EMERGENCY = 2
 BTN_TAKEOFF = 5
@@ -21,12 +21,19 @@ AXIS_RIGHT_LEFT = 0
 AXIS_UP_DOWN = 2
 AXIS_ORIENTATION = 3
 
-resetPub = rospy.Publisher('bebop/reset', Empty, queue_size=10)
-takeoffPub = rospy.Publisher('bebop/takeoff', Empty, queue_size=10)
-landPub = rospy.Publisher('bebop/land', Empty, queue_size=10)
-flipPub = rospy.Publisher('bebop/flip', UInt8, queue_size=10)
+resetPub = rospy.Publisher('bebop/reset', Empty, queue_size=0)
+takeoffPub = rospy.Publisher('bebop/takeoff', Empty, queue_size=0)
+landPub = rospy.Publisher('bebop/land', Empty, queue_size=0)
+flipPub = rospy.Publisher('bebop/flip', UInt8, queue_size=0)
 
 pilotPub = rospy.Publisher('bebop/cmd_vel', Twist, queue_size=10)
+
+# mode 0 : loisir
+# mode 1 : surveillance
+LOISIR = 0
+SURVEILLANCE = 1
+
+mode = LOISIR 
 
 
 def controller():
@@ -40,7 +47,21 @@ def controller():
 
     rospy.Subscriber('/joy', Joy, callback)
 
-    
+    s1 = rospy.Service('modeLoisir', mode_loisir, modeLoisir)
+    s2 = rospy.Service('modeSurveillance', mode_surveillance, modeSurveillance)
+
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        
+        if mode == SURVEILLANCE:
+            hello_str = "surveillance..."
+            rospy.loginfo(hello_str)
+            linear = Vector3(0, 0, 0)
+            angular = Vector3(0, 0, 0.5)
+            twist = Twist(linear, angular)
+            rospy.loginfo(rospy.get_caller_id() + 'tourelle : %s', str(twist))
+            pilotPub.publish(twist)
+        rate.sleep()
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -48,51 +69,70 @@ def controller():
 def callback(data):
     joy = data
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', str(joy))
-
+    
     #order is important : priority if several buttons pressed at once
+
     if joy.buttons[BTN_EMERGENCY]:
         resetPub.publish()
         rospy.loginfo(rospy.get_caller_id() + 'emergency')
+        return
     elif joy.buttons[BTN_LAND]:
         landPub.publish()
         rospy.loginfo(rospy.get_caller_id() + 'land')
+        return
     elif joy.buttons[BTN_TAKEOFF]:
         takeoffPub.publish()
         rospy.loginfo(rospy.get_caller_id() + 'takeoff')
-    elif joy.axes[AXIS_FORWARD_BACKWARD] != 0:
-        linear = Vector3(joy.axes[AXIS_FORWARD_BACKWARD], 0, 0)
-        angular = Vector3(0, 0, 0)
-        twist = Twist(linear, angular)
-        pilotPub.publish(twist)
-        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
-    elif joy.axes[AXIS_RIGHT_LEFT] != 0:
-        linear = Vector3(0, joy.axes[AXIS_RIGHT_LEFT], 0)
-        angular = Vector3(0, 0, 0)
-        twist = Twist(linear, angular)
-        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
-        pilotPub.publish(twist)
-    elif joy.axes[AXIS_UP_DOWN] != 0:
-        linear = Vector3(0, 0, joy.axes[AXIS_UP_DOWN])
-        angular = Vector3(0, 0, 0)
-        twist = Twist(linear, angular)
-        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
-        pilotPub.publish(twist)
-    elif joy.axes[AXIS_ORIENTATION] != 0:
-        linear = Vector3(0, 0, 0)
-        angular = Vector3(0, 0, joy.axes[AXIS_ORIENTATION])
-        twist = Twist(linear, angular)
-        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
-        pilotPub.publish(twist)
-    elif joy.buttons[BTN_FRONTFLIP] != 0:
-        flipPub.publish(0)
-        rospy.loginfo(rospy.get_caller_id() + 'backflip')
+        return
 
-
-
+    if (mode == LOISIR):
+        if joy.axes[AXIS_FORWARD_BACKWARD] != 0:
+            linear = Vector3(joy.axes[AXIS_FORWARD_BACKWARD], 0, 0)
+            angular = Vector3(0, 0, 0)
+            twist = Twist(linear, angular)
+            pilotPub.publish(twist)
+            rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+        elif joy.axes[AXIS_RIGHT_LEFT] != 0:
+            linear = Vector3(0, joy.axes[AXIS_RIGHT_LEFT], 0)
+            angular = Vector3(0, 0, 0)
+            twist = Twist(linear, angular)
+            rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+            pilotPub.publish(twist)
+        elif joy.axes[AXIS_UP_DOWN] != 0:
+            linear = Vector3(0, 0, joy.axes[AXIS_UP_DOWN])
+            angular = Vector3(0, 0, 0)
+            twist = Twist(linear, angular)
+            rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+            pilotPub.publish(twist)
+        elif joy.axes[AXIS_ORIENTATION] != 0:
+            linear = Vector3(0, 0, 0)
+            angular = Vector3(0, 0, joy.axes[AXIS_ORIENTATION])
+            twist = Twist(linear, angular)
+            rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+            pilotPub.publish(twist)
+        elif joy.buttons[BTN_FRONTFLIP] != 0:
+            flipPub.publish(0)
+            rospy.loginfo(rospy.get_caller_id() + 'backflip')
+        elif joy.buttons[BTN_MODE] != 0:
+            flipPub.publish(0)
+            rospy.loginfo(rospy.get_caller_id() + 'backflip')
     
+
+
+
+def modeLoisir(data):
+    global mode 
+    mode = LOISIR
+    rospy.loginfo(rospy.get_caller_id() + 'Now in LOISIR mode')
+    return True
+
+def modeSurveillance(data):
+    global mode
+    mode = SURVEILLANCE
+    rospy.loginfo(rospy.get_caller_id() + 'Now in SURVEILLANCE mode')
+    return True
+
         
-
-
 
 if __name__ == '__main__':
     try:
