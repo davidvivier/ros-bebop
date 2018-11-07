b3bea165aebfdc44529600d639de33c680c36983
David Vivier <david.vivier@cpe.fr>
2018-11-07 15:23:47 +0100

    Add commands right/left, up/down, orientation, frontflip

--- src/mini_projet/scripts/control.py
+++ src/mini_projet/scripts/control.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Empty
+from std_msgs.msg import UInt8
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
@@ -12,6 +13,7 @@ BTN_EMERGENCY = 2
 BTN_TAKEOFF = 5
 BTN_LAND = 4
 BTN_MODE = 0
+BTN_FRONTFLIP = 3
 
 AXIS_FORWARD_BACKWARD = 1
 AXIS_RIGHT_LEFT = 0
@@ -22,6 +24,7 @@ AXIS_ORIENTATION = 3
 resetPub = rospy.Publisher('bebop/reset', Empty, queue_size=10)
 takeoffPub = rospy.Publisher('bebop/takeoff', Empty, queue_size=10)
 landPub = rospy.Publisher('bebop/land', Empty, queue_size=10)
+flipPub = rospy.Publisher('bebop/flip', UInt8, queue_size=10)
 
 pilotPub = rospy.Publisher('bebop/cmd_vel', Twist, queue_size=10)
 
@@ -44,19 +47,47 @@ def controller():
 
 def callback(data):
     joy = data
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', str(joy.buttons[BTN_EMERGENCY]))
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', str(joy))
 
-
-    # order is important : priority if several buttons pressed at once
+    #order is important : priority if several buttons pressed at once
     if joy.buttons[BTN_EMERGENCY]:
         resetPub.publish()
+        rospy.loginfo(rospy.get_caller_id() + 'emergency')
     elif joy.buttons[BTN_LAND]:
         landPub.publish()
+        rospy.loginfo(rospy.get_caller_id() + 'land')
     elif joy.buttons[BTN_TAKEOFF]:
         takeoffPub.publish()
+        rospy.loginfo(rospy.get_caller_id() + 'takeoff')
     elif joy.axes[AXIS_FORWARD_BACKWARD] != 0:
-        twist = Twist(Vector3(joy.axes[AXIS_FORWARD_BACKWARD], 0, 0), Vector3(0,0,0))
+        linear = Vector3(joy.axes[AXIS_FORWARD_BACKWARD], 0, 0)
+        angular = Vector3(0, 0, 0)
+        twist = Twist(linear, angular)
         pilotPub.publish(twist)
+        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+    elif joy.axes[AXIS_RIGHT_LEFT] != 0:
+        linear = Vector3(0, joy.axes[AXIS_RIGHT_LEFT], 0)
+        angular = Vector3(0, 0, 0)
+        twist = Twist(linear, angular)
+        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+        pilotPub.publish(twist)
+    elif joy.axes[AXIS_UP_DOWN] != 0:
+        linear = Vector3(0, 0, joy.axes[AXIS_UP_DOWN])
+        angular = Vector3(0, 0, 0)
+        twist = Twist(linear, angular)
+        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+        pilotPub.publish(twist)
+    elif joy.axes[AXIS_ORIENTATION] != 0:
+        linear = Vector3(0, 0, 0)
+        angular = Vector3(0, 0, joy.axes[AXIS_ORIENTATION])
+        twist = Twist(linear, angular)
+        rospy.loginfo(rospy.get_caller_id() + 'pilot : %s', str(twist))
+        pilotPub.publish(twist)
+    elif joy.buttons[BTN_FRONTFLIP] != 0:
+        flipPub.publish(0)
+        rospy.loginfo(rospy.get_caller_id() + 'backflip')
+
+
 
     
         
